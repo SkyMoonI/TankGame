99cecbbe2afafae2678d12aeb93b7f57aa3eee46
Gokay Iseri <gokay.iseri@outlook.com>
2024-08-11 19:16:03 +0300

    Simple Level Selections Scene Created

---
+++ Turbo Z-Tank/Assets/Project/Scripts/LevelSelection/LevelSelectionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LevelSelectionManager : MonoBehaviour
+{
+	[SerializeField] List<Button> _levelButtons;
+
+	private void Start()
+	{
+		for (int i = 0; i < _levelButtons.Count; i++)
+		{
+			Button button = _levelButtons[i];
+
+			int capturedIndex = i;
+			capturedIndex++;
+			Debug.Log(capturedIndex);
+
+			button.onClick.AddListener(() => SelectLevel(capturedIndex));
+			// Example of locking a level until the previous level is completed
+			if (PlayerPrefs.GetInt("Level" + capturedIndex + "Completed", 0) == 0)
+			{
+				if (capturedIndex < _levelButtons.Count)
+				{
+					_levelButtons[capturedIndex].interactable = false;
+
+				}
+			}
+		}
+	}
+	public void SelectLevel(int level)
+	{
+		SceneManager.LoadScene("Level" + level);
+	}
+
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenuScene");
+	}
+}
